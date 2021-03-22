03df0ded7f37f6539602d1e41e3f157e56ac3537
Monisha E <monisha.e@ctlep.local>
2021-03-22 15:25:39 +0530

    fifth commit

--- hello.php
+++ hello.php
@@ -1,5 +1,7 @@
 <?php  //foreach array
 $array=('male,female');
 foreach($array as $row){?> 
-<option value="<?php echo $row; ?>"><?php echo $row;?> </option> <?php }
+<option value="<?php echo $row; ?>"><?php echo $row;?> </option> <?php  }
+  $html_msg  ="hello";
+  echo $html_msg;
  ?>
