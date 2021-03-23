53fb0bdbf1639ca69a02407596bb829770c12b1d
Monisha E <monisha.e@ctlep.local>
2021-03-23 13:02:50 +0530

    new commit

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 <p id="demo">JavaScript can change HTML content.</p>
 
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
-
+<div class="alert alert-success alert-dismissible" role="alert">
+	<button type="button" class="close" data-dismiss="alert"
+		aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	</button>
+	<?php echo "Welcome"; ?>
+</div>
 </body>
 </html>
