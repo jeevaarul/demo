91864a7b110e192688ff6d9d332966485bdcda04
Monisha E <monisha.e@ctlep.local>
2021-03-24 10:18:51 +0530

    new changes

--- hello.php
+++ hello.php
@@ -1,9 +1,1 @@
-<?php  //foreach array
-$array=('male,female');
-foreach($array as $row){?> 
-<option value="<?php echo $row; ?>"><?php echo $row;?> </option> <?php  }
-  $html_msg  ="hello";
-  echo $html_msg;
-  $sql="SELECT * FROM table_name";
-  echo $sql;
- ?>
+<?php echo "Sample"; echo 'new'; ?>
